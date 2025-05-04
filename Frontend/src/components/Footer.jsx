319d930443564c5f05395c00883949b8187d3ee6
Dhruv280704 <Dhruvking425@gmail.com>
2025-05-04 19:28:36 +0530

    first commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Dhruv Mishra
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ Frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const WorkoutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+        Push yourself every day.
+        Stay strong, stay steady.
+        Your future self will thank you.
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+        At Ironstack Fitness, we don’t just lift weights — we lift spirits, crush limits, and build warriors who never back down.
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Push yourself — no one else will.
+            Be your own motivation.</h4>
+            <p>
+            Every rep you push, every drop of sweat you shed, is a step closer to the strongest version of yourself. Ironstack is where that transformation begins.
+            </p>
+          </div>
+          <div>
+            <h4>Strive for progress, not perfection.
+            Consistency is key.</h4>
+            <p>
+            Discipline, drive, and determination — that’s what stacks up at Ironstack. And that’s what sets our warriors apart from the rest.
+            </p>
+          </div>
+          <div>
+            <h4>Fitness begins in the mind.
+            Believe. Achieve. Repeat.</h4>
+            <p>
+            When the weights feel heavy, remember: so did your doubts. But you’re stronger now. Ironstack made sure of that.
+            </p>
+          </div>
+          <div>
+            <h4>Your only limit is you.
+            Break barriers, build muscle.</h4>
+            <p>
+            This is the place where sweat becomes strength, and failure becomes fuel. Ironstack Fitness — built for those who refuse to settle."
+            </p>
+          </div>
+          <div>
+            <h4>Strong is the new fit.
+            Live fit, love life.</h4>
+            <p>You didn’t come this far to be average. At Ironstack, we break through comfort zones and build unshakable confidence, inside and out.</p>
+          </div>
+          <div>
+            <h4>Fuel your body, feel your best.
+              Small steps, big changes.
+            </h4>
+            <p>Strength isn’t given — it’s earned. And here at Ironstack Fitness, you earn it one rep at a time, through focus, fire, and relentless grind.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSessions;
